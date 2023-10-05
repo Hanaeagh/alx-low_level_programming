7283b935432572b4b71772b95a9bab722624fe3f
Hanaeagh <hanaeaghoutane@gmail.com>
2023-10-05 11:07:31 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
-#include <stdio>
-
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: a c program that prints with put function
- *
- * Return Always 0 (Success)
+* main - Entry point
+*
+* Description: This is a C program that uses the puts function
+* to print a message to the standard output.
+*
+* Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	puts("\ "Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
